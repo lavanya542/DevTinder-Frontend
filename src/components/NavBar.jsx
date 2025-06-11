e4592051a96d8507636cb82775e209a460afefcd
Rokkam <rokkamanu542@gmail.com>
2025-06-11 18:36:46 +0530

    Finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar=()=>{
         <div className="navbar bg-base-100 shadow-sm">
          
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevTinder</a>
+    <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
   </div>
   {user&&(
   <div className="flex gap-2">
